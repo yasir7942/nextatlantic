35daadb33fb0af2ba56a8583cf3e7cc3cf4f31cb
yasir aslam <yasir7942@outlook.com>
2024-12-10 11:14:41 +0500

    spelling mistake in menu

--- app/components/layout/menu-list.js
+++ app/components/layout/menu-list.js
@@ -8,7 +8,7 @@ const MenuList = () => {
         <ul className="hidden md:flex text-gray-200 md:space-x-3 lg:space-x-6  text-lg md:font-light  lg:font-normal capitalize tracking-wider ">
             <li><Link href={`/`} > Home</Link></li>
             <li><Link href={`/about-us`} > About Us</Link></li>
-            <li> <Link href={`/products`} > Product </Link></li>
+            <li> <Link href={`/products`} > Products </Link></li>
             <li><Link href={`/blog`}  > Blog </Link></li>
             <li><Link href={`/contact`}   > Contact Us</Link></li>
         </ul>
--- app/components/layout/mobile-nav.js
+++ app/components/layout/mobile-nav.js
@@ -1,72 +1,72 @@
 "use client";
 
-import { useState } from 'react'; 
+import { useState } from 'react';
 import { IoMdMenu } from "react-icons/io";
- 
+
 
 import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-  } from "@/components/ui/sheet"
-  import Link from "next/link";
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import Link from "next/link";
 import SocialIcons from '../elements/social-icons';
 import siteConfig from '@/config/site';
 import Image from 'next/image';
-  
+
 
 
 const MobileNavigation = () => {
 
-    const [openSheet, setOpenSheet] = useState(false);
+  const [openSheet, setOpenSheet] = useState(false);
 
 
 
   return (
     <div>
       <Sheet open={openSheet} onOpenChange={setOpenSheet}>
-            <SheetTrigger asChild><a href="#"><IoMdMenu aria-label={`Mobile Menu`}  size={32} /> </a></SheetTrigger>
-            <SheetContent className="w-72">
-              <SheetHeader>
+        <SheetTrigger asChild><a href="#"><IoMdMenu aria-label={`Mobile Menu`} size={32} /> </a></SheetTrigger>
+        <SheetContent className="w-72">
+          <SheetHeader>
 
-                <SheetDescription >
+            <SheetDescription >
 
-                  <div className="flex flex-col w-full h-full justify-between items-center py-6 px-4 relative     ">
+              <div className="flex flex-col w-full h-full justify-between items-center py-6 px-4 relative     ">
 
-                    <a href="/" className="w-[80%]">
-                    <Image
-                        src= "/images/logo-332x88.png"
-                        width={332}
-                        height={88}
-                        alt="Atlantic Grease and Lubricants logo" />
-                    </a>
-
-
-                    <ul className="flex flex-col text-gray-200 mt-10 space-y-3 text-center  text-lg  font-light capitalize tracking-wider ">
-                      <li><Link href={`/`} onClick={() => {setOpenSheet(false)}} > Home</Link></li>
-                      <li><Link href={`/about-us`} onClick={() => {setOpenSheet(false)}} > About Us</Link></li>
-                      <li> <Link href={`/products`} onClick={() => {setOpenSheet(false)}} > Product </Link></li>
-                      <li><Link href={`/blog`}  onClick={() => {setOpenSheet(false)}} > Blog </Link></li>
-                      <li><Link href={`/contact`}  onClick={() => {setOpenSheet(false)}}  >Contact Us</Link></li>
-                    </ul>
-
-                    <ul className="flex text-white space-x-7 mt-10 ">
-                      <li><SocialIcons plateform="facebook" link={siteConfig.socialMedia.facebook} /></li>
-                      <li><SocialIcons plateform="instagram" link={siteConfig.socialMedia.instagram} /></li>
-                      <li><SocialIcons plateform="twitter" link={siteConfig.socialMedia.twitter} /></li>
-                      <li><SocialIcons plateform="youtube" link={siteConfig.socialMedia.youtube} /></li>
-                    </ul>
-
-                  </div>
+                <a href="/" className="w-[80%]">
+                  <Image
+                    src="/images/logo-332x88.png"
+                    width={332}
+                    height={88}
+                    alt="Atlantic Grease and Lubricants logo" />
+                </a>
 
 
-                </SheetDescription>
-              </SheetHeader>
-            </SheetContent>
-          </Sheet>
+                <ul className="flex flex-col text-gray-200 mt-10 space-y-3 text-center  text-lg  font-light capitalize tracking-wider ">
+                  <li><Link href={`/`} onClick={() => { setOpenSheet(false) }} > Home</Link></li>
+                  <li><Link href={`/about-us`} onClick={() => { setOpenSheet(false) }} > About Us</Link></li>
+                  <li> <Link href={`/products`} onClick={() => { setOpenSheet(false) }} > Products </Link></li>
+                  <li><Link href={`/blog`} onClick={() => { setOpenSheet(false) }} > Blog </Link></li>
+                  <li><Link href={`/contact`} onClick={() => { setOpenSheet(false) }}  >Contact Us</Link></li>
+                </ul>
+
+                <ul className="flex text-white space-x-7 mt-10 ">
+                  <li><SocialIcons plateform="facebook" link={siteConfig.socialMedia.facebook} /></li>
+                  <li><SocialIcons plateform="instagram" link={siteConfig.socialMedia.instagram} /></li>
+                  <li><SocialIcons plateform="twitter" link={siteConfig.socialMedia.twitter} /></li>
+                  <li><SocialIcons plateform="youtube" link={siteConfig.socialMedia.youtube} /></li>
+                </ul>
+
+              </div>
+
+
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
 
     </div>
   )
